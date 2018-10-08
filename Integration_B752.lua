0bbe063ddd20eb442fd833b423d9406b9d22f3ce
Linus <43395182+AceHawk75@users.noreply.github.com>
2018-10-07 21:53:54 -0400

    Add files via upload
    B752 
    - re-write of the Auto Throttle toggle button
    - Added check for XPlane A/T engage status -- possible cause of disagreement XP11 vs B752 vs CFY - still TBD
    - New A/T Disengage logic
    - New A/P Disengage logic

--- Integration_B752.lua
+++ Integration_B752.lua
@@ -7,17 +7,32 @@ if (PLANE_ICAO =="B752") then
 	FieldOfView = dataref_table("sim/graphics/view/field_of_view_deg")
 	FieldOfView[0] = 75
 
+	-- Joystick Assignments - Jets
+	clear_all_axis_assignments()
+	set_axis_assignment( 50, "pitch", "normal" )
+	set_axis_assignment( 51, "roll", "normal" )
+	set_axis_assignment( 75, "right toe brake", "normal" )
+	set_axis_assignment( 76, "left toe brake", "normal" )
+	set_axis_assignment( 77, "yaw", "normal" )
+
+	clear_all_button_assignments()
+	set_button_assignment( (0*40) + 5, "sim/flight_controls/landing_gear_up" )
+	set_button_assignment( (0*40) + 6, "sim/flight_controls/landing_gear_down" )
+	
+	
+	--COMMENTED OUT 10/2/2018
 -- Start Override Throttle off
 -- Turns off the override of the throttles
 	Override_Throttle = dataref_table("sim/operation/override/override_throttles")
-	function FxOverrideThrottles ()
-		if Override_Throttle[0] ~= 0 then
-			Override_Throttle[0] = 0
-		end
+--	function FxOverrideThrottles ()
+--		if Override_Throttle[0] ~= 0 then
+--			Override_Throttle[0] = 0
+--		end
 	--print(Override_Throttle[0])
-	end
-	do_often("FxOverrideThrottles()")
+--	end
+--	do_often("FxOverrideThrottles()")
 -- End Override Throttle off
+--COMMENTED OUT 10/2/2018
 
 --Start MouseHandler always on
 --	MouseHandler = dataref_table("mh/mouse_handler_on_window")
@@ -139,8 +154,22 @@ if (PLANE_ICAO =="B752") then
 	do_often("updateB752StartLever2()")
 -- End StartLever1
 
-
+--COMMENTED OUT 10/2/2018
 -- Start AT_Disengage
+-- First: command to disengage A/T
+	create_command("linus/B752/AutoThrottle_Disengage","B752 AutoThrottle Disengage",
+			-- OFF = 1
+			-- ON = 0
+			[[
+				-- set AutoThrottle = OFF
+				B752_AT_arm_sw = 1
+				command_once("sim/autopilot/autothrottle_off")
+				B752_AT_VALUE[0] = 1
+			]],
+		 "",
+		 "")
+
+
 -- Checks the state of the CFY AT Disengage and adjusts the B757-200 A/T State accordingly
 	dataref("AT_Disen_752","linus/CFY/ATDisen","writeable",0)
 	
@@ -152,12 +181,47 @@ if (PLANE_ICAO =="B752") then
 	--		AT_Value[0] = -1
 	--		AT_arm_sw = 0
 	--		XP_AT_Enabled[0] = 0
-			command_once("linus/B752/ToggleAutoThrottle")
+	--		command_once("linus/B752/ToggleAutoThrottle")
+			command_once("linus/B752/AutoThrottle_Disengage")
 			AT_Disen_752 = 1
 		end
 	end
 	do_often("updateB752ATDisen()")
 -- End AT_Disengage
+--COMMENTED OUT 10/2/2018
+
+
+
+-- Start A/P Disengage button
+-- Disengages the A/P (via the MCP lever)
+	dataref("B752_AP_Disengage", "1-sim/AP/desengageLever", "writeable")
+	B752_AT_State = 0
+	create_command("linus/B752/AP_Disengage","Disengage AutoPilot",
+			-- OFF = 1
+			-- ON = 0
+			[[
+				
+				
+				-- set AutoPilot = OFF
+				B752_AT_State = B752_AT_State + 1
+				B752_AP_Disengage = 1
+				--sets off alarm - on second press we return bar to on (turns off alarm)
+				--sleep(3)
+				--B752_AP_Disengage = 0
+				if B752_AT_State >=2 then 
+					B752_AT_State = 0
+					B752_AP_Disengage = 0
+				else
+				
+				end 
+			]],
+		 "",
+		 "")
+
+-- End A/P Disengage button
+	
+-- Sets the button for the A/P disengage
+	set_button_assignment( (8*40) + 4, "linus/B752/AP_Disengage" )
 
 -- Start Speed Brake lever
 -- Checks the state of the CFY Speed Brake Lever and adjusts the B752 Speed Brake Lever accordingly
@@ -189,17 +253,7 @@ if (PLANE_ICAO =="B752") then
 --End XpndrMode
 
 
-	-- Joystick Assignments - Jets
-	clear_all_axis_assignments()
-	set_axis_assignment( 50, "pitch", "normal" )
-	set_axis_assignment( 51, "roll", "normal" )
-	set_axis_assignment( 75, "right toe brake", "normal" )
-	set_axis_assignment( 76, "left toe brake", "normal" )
-	set_axis_assignment( 77, "yaw", "normal" )
 
-	clear_all_button_assignments()
-	set_button_assignment( (0*40) + 5, "sim/flight_controls/landing_gear_up" )
-	set_button_assignment( (0*40) + 6, "sim/flight_controls/landing_gear_down" )
 
 -- code for B752 taxi lights
 -- first define the three lights here
@@ -264,59 +318,128 @@ if (PLANE_ICAO =="B752") then
 	set_button_assignment( (8*40) + 3, "linus/B752/ToggleLandingLights" )
 -- end B752 Landing Lights code
 
-	set_button_assignment( (8*40) + 6, "sim/flight_controls/pitch_trim_down" )
-	set_button_assignment( (8*40) + 8, "sim/flight_controls/pitch_trim_up" )
+--COMMENTED OUT 10/2/2018
+--	set_button_assignment( (8*40) + 6, "sim/flight_controls/pitch_trim_down" )
+--	set_button_assignment( (8*40) + 8, "sim/flight_controls/pitch_trim_up" )
+--COMMENTED OUT 10/2/2018
+
+--New A/T logic OCT 2018
+AT_FLCH = dataref_table("1-sim/AP/flchButton")
+AT_SPD = dataref_table("1-sim/AP/spdButton")
+AT_VNAV = dataref_table("1-sim/AP/vnavButton")
+AT_EPR = dataref_table("1-sim/AP/eprButton")
 
 
+
+
+--COMMENTED OUT 10/2/2018
 -- code for B752 AutoThrottle switch
 -- first define the switch
-	dataref("AT_arm_sw", "1-sim/AP/atSwitcher", "writeable")  -- 0 = on, 1 = OFF
+--	dataref("AT_arm_sw", "1-sim/AP/atSwitcher", "writeable")  -- 0 = on, 1 = OFF
 -- define the other A/T stuff here
-	XP_AT_Enabled = dataref_table("sim/cockpit2/autopilot/autothrottle_enabled")
-	AT_SPD = dataref_table("1-sim/AP/spdButton")
+--	XP_AT_Enabled = dataref_table("sim/cockpit2/autopilot/autothrottle_enabled")
+--	AT_SPD = dataref_table("1-sim/AP/spdButton")
 	
 -- define the value state	
-	define_shared_DataRef("linus/B752/ATValue","Int")
-	AT_value = dataref_table("linus/B752/ATValue")
+--	define_shared_DataRef("linus/B752/ATValue","Int")
+--	AT_value = dataref_table("linus/B752/ATValue")
 	-- at start, AT off
-		AT_value[0] = -1
-		AT_arm_sw = 1
-		XP_AT_Enabled[0] = 0
-		AT_Disen_752 = 1
+--		AT_value[0] = -1
+--		AT_arm_sw = 1
+--		XP_AT_Enabled[0] = 0
+--		AT_Disen_752 = 1
 	
 	-- command for button
-	create_command("linus/B752/ToggleAutoThrottle","Toggle B752 AutoThrottle",
-		[[if AT_value[0] == -1 then		
-			-- set AutoThrottle = off
-			--AT_arm_sw = 1
-			XP_AT_Enabled[0] = 0
-			-- turn off the A/T buttons before the A/T switch!
-			AT_FLCH[0] = 0
-			AT_SPD[0] = 0
-			AT_VNAV[0] = 0
-			AT_arm_sw = 1
-			AT_Disen_752 = 1
-		else 	
-			-- set AutoThrottle = on
-			AT_arm_sw = 0
-			-- insert the code to arm the AT here
-			if AT_FLCH[0] == 1 then
-				XP_AT_Enabled[0] = 1
-			end
-			if AT_SPD[0] == 1 then
-				XP_AT_Enabled[0] = 1
-			end
-			if AT_VNAV[0] == 1 then
-				XP_AT_Enabled[0] = 1
-			end
-			AT_Disen_752 = 1
-		end
-		AT_value[0] = AT_value[0] *-1]],
-		"",
-		"")
+--	create_command("linus/B752/ToggleAutoThrottle","Toggle B752 AutoThrottle",
+		-- [[if AT_value[0] == -1 then		
+			-- -- set AutoThrottle = off
+			-- --AT_arm_sw = 1
+			-- XP_AT_Enabled[0] = 0
+			-- -- turn off the A/T buttons before the A/T switch!
+			-- --AT_FLCH[0] = 0
+			-- --AT_SPD[0] = 0
+			-- --AT_VNAV[0] = 0
+			-- AT_arm_sw = 1
+			-- AT_Disen_752 = 1
+		-- else 	
+			-- -- set AutoThrottle = on
+			-- AT_arm_sw = 0
+			-- -- insert the code to arm the AT here
+			-- --if AT_FLCH[0] == 1 then
+			-- --	XP_AT_Enabled[0] = 1
+			-- end
+			-- --if AT_SPD[0] == 1 then
+			-- --	XP_AT_Enabled[0] = 1
+			-- end
+			-- --if AT_VNAV[0] == 1 then
+			-- --	XP_AT_Enabled[0] = 1
+			-- end
+			-- AT_Disen_752 = 1
+		-- end
+		-- AT_value[0] = AT_value[0] *-1]],
+		-- "",
+		-- "")
 	-- button assignment
-	set_button_assignment( (8*40) + 14, "linus/B752/ToggleAutoThrottle" )
--- end B752 Auto Throttle switch
+--	set_button_assignment( (8*40) + 14, "linus/B752/ToggleAutoThrottle" )
+	dataref("B752_AT_arm_sw", "1-sim/AP/atSwitcher", "writeable")  -- 0 = ON, 1 = OFF
+	define_shared_DataRef("linus/B752/ATValue","Int")
+	B752_AT_VALUE = dataref_table("linus/B752/ATValue")
+	
+	
+	--local B752_AT_VALUE
+	B752_AT_VALUE[0] = -1
+	
+	
+--to inspect values of the AT MCP button states
+function AT_MCP_Buttons()
+--	print("AT_EPR: " .. AT_EPR[0] .. ", AT_FLCH: " .. AT_FLCH[0] .. ", AT_SPD: " .. AT_SPD[0] .. ", AT_VNAV: " .. AT_VNAV[0] .. ", AT_EPR: " .. AT_EPR)
+--	print("B752_AT_VALUE: " .. B752_AT_VALUE[0] .. ", B752_AT_arm_sw: " .. B752_AT_arm_sw)
+end
+do_often("AT_MCP_Buttons()")
+
+	create_command("linus/B752/ToggleAutoThrottle","Toggle B752 AutoThrottle",
+			-- OFF = 1
+			-- ON = 0
+			[[
+			
+			if B752_AT_VALUE[0] == 0 then		
+				-- set AutoThrottle = OFF
+				B752_AT_arm_sw = 1
+				command_once("sim/autopilot/autothrottle_off")
+				B752_AT_VALUE[0] = 1
+			else 	
+				-- set AutoThrottle = ON
+				B752_AT_arm_sw = 0
+				command_once("sim/autopilot/autothrottle_on")
+				B752_AT_VALUE[0] = 0
+			end
+			]],
+		 "",
+		 "")
+-- key: sim/cockpit2/autopilot/autothrottle_enabled
+--	set_button_assignment( (8*40) + 14, "sim/autopilot/autothrottle_toggle" )
+	set_button_assignment( (8*40) + 14, "linus/B752/ToggleAutoThrottle" )	
+	-- end B752 Auto Throttle switch
+
+-- Start Override Throttle off
+-- Ensures Auto Throttle stays on if switch is on the on state
+	dataref("B752_Override_Throttle", "sim/cockpit2/autopilot/autothrottle_enabled", "writeable")
+	function B752_FxOverrideThrottles ()
+		--first check if the AT_switch is on
+		if B752_AT_arm_sw == 0 then
+			--switch is on - so override should be on
+			if B752_Override_Throttle == 0 then
+				--command_once("sim/autopilot/autothrottle_on")
+				B752_Override_Throttle = 1
+			end
+		else
+		end
+--			print("B752_Override_Throttle: " .. B752_Override_Throttle .. ", B752_AT_arm_sw: " .. B752_AT_arm_sw)
+	end
+	do_often("B752_FxOverrideThrottles()")
+-- End Override Throttle off
+
+
 
 -- code for B752 Altimeter Barometer Setting UP
 -- first define the dataref
@@ -365,55 +488,59 @@ if (PLANE_ICAO =="B752") then
 	set_button_assignment( (0*40) + 2, "linus/B752/BarometerSync" )
 -- end B752 Sync Altimeter Barometer Setting code
 
-
+--COMMENTED OUT 10/2/2018
 	-- B752 A/T FLCH switch and CFY
 	--IF A/T is on then 
 		--If FLCH is turned on, we want AT to remain on (AND CFY to continue working)
 	--ELSE
 	--	We want FLCH to turn off 
 	--dataref("AT_FLCH","1-sim/AP/flchButton","readonly")
-	AT_FLCH = dataref_table("1-sim/AP/flchButton")
-	function CheckFLCH()
-		if AT_arm_sw == 0 then
-			if	AT_FLCH[0] == 1 then
-				XP_AT_Enabled[0] = 1
-			end
-		else
-			AT_FLCH[0] = 0
-		end
-	end
-	do_often ("CheckFLCH()")
+--	AT_FLCH = dataref_table("1-sim/AP/flchButton")
+--	function CheckFLCH()
+--		if AT_arm_sw == 0 then
+--			if	AT_FLCH[0] == 1 then
+--				XP_AT_Enabled[0] = 1
+--			end
+--		else
+--			AT_FLCH[0] = 0
+--		end
+--	end
+--	do_often ("CheckFLCH()")
 	-- end FLCH
 
 	-- B752 A/T VNAV switch and CFY
 	-- If VNAV is turned on, we want AT to remain on (AND CFY to continue working)
 	--dataref("AT_VNAV","1-sim/AP/vnavButton","readonly")
-	AT_VNAV = dataref_table("1-sim/AP/vnavButton")
-	function CheckVNAV()
-		if AT_arm_sw == 0 then
-			if	AT_VNAV[0] == 1 then
-				XP_AT_Enabled[0] = 1
-			end
-		end
-	end
-	do_often ("CheckVNAV()")
+--	AT_VNAV = dataref_table("1-sim/AP/vnavButton")
+--	function CheckVNAV()
+--		if AT_arm_sw == 0 then
+--			if	AT_VNAV[0] == 1 then
+--				XP_AT_Enabled[0] = 1
+--			end
+--		--added 10/2/2018
+--		else	
+--			AT_VNAV[0] = 0
+--		end
+--	end
+--	do_often ("CheckVNAV()")
 	-- end VNAV check
 
 	-- B752 A/T SPD switch and CFY
 	-- If SPD is turned on, we want AT to remain on (AND CFY to continue working)
 	--dataref("AT_VNAV","1-sim/AP/vnavButton","readonly")
-	function CheckSPD()
-		if AT_arm_sw == 0 then
-			if	AT_SPD[0] == 1 then
-				XP_AT_Enabled[0] = 1
-			end
-		else	
-			AT_SPD[0] = 0
-		end
-	end
-	do_often ("CheckSPD()")
-	-- end VNAV check
+--	function CheckSPD()
+--		if AT_arm_sw == 0 then
+--			if	AT_SPD[0] == 1 then
+--				XP_AT_Enabled[0] = 1
+--			end
+--		else	
+--			AT_SPD[0] = 0
+--		end
+--	end
+--	do_often ("CheckSPD()")
+	-- end SPD check
 
+--COMMENTED OUT 10/2/2018
 
 --set_button_assignment( (8*40) + 14, "sim/autopilot/autothrottle_toggle" )
 
